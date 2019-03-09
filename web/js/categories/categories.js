921436a6cfe0193d832645cf186ca7b486c16e7c
Dmitriy <dimakoryak@gmail.com>
2019-03-09 19:25:31 +0300

    add anim affect in categories

--- web/js/categories/categories.js
+++ web/js/categories/categories.js
@@ -3,6 +3,7 @@
         anim.elem('h1', 'animated fadeInUp');
         anim.elem('#dk_filter', 'animated delay-05s fadeInLeft');
         anim.elemsCascade('#goods_show', '.good_show', 'fadeInDown', 3, 0);
+        anim.elemsCascade('#main_categories_container', '.category', 'fadeInDown', 3, 0);
         anim.elem('.breadcrumb', 'animated flipInX');
 
         // если чистый адрес без id категории то прикрепляем анимацию dk_sidenav
