c90ec8f8006e16ff1f10bf24150dc9ace87ee651
Dmitriy <dimakoryak@gmail.com>
2019-04-12 21:14:22 +0300

    Добавлены алиасы, модальное окно корзины и телефоны - fix1

--- views/cart/modal.php
+++ views/cart/modal.php
@@ -5,22 +5,22 @@
  * Date: 09.04.19
  * Time: 21:50
  */
+
 use yii\bootstrap\Modal;
 use yii\helpers\Url;
+use yii\widgets\Pjax;
 
 //<!-- Modal -->
-
 Modal::begin([
-    'header' => "<h3>У Вас в корзине " . Yii::$app->cart->getCount() . " товар(ов)</h3>",
+    'header' => "<h3>У Вас в корзине есть товары</h3>",
     'id' => 'cart_modal',
 ]);
-
 ?>
 
-<div>
-    <button data-dismiss="modal" class="btn btn-success">Продолжить покупки</button>
-    <a href="<?= Url::to(['/cart/show'])?>" class="btn btn-primary">Перейти к оформлению заказа</a>
-</div>
+    <div>
+        <button data-dismiss="modal" class="btn btn-success">Продолжить покупки</button>
+        <a href="<?= Url::to(['/cart/show']) ?>" class="btn btn-primary">Перейти к оформлению заказа</a>
+    </div>
 
 <?php
 Modal::end();
