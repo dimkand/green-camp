a667b6351b40ee3202187a2648c63aa5ceda26db
Dmitriy <dimakoryak@gmail.com>
2019-05-03 09:37:17 +0300

    _

--- components/Helpers.php
+++ components/Helpers.php
@@ -126,7 +126,7 @@ class Helpers
 
         $str = strtr($str, $converter);// переводим в транслит
         $str = strtolower($str);// в нижний регистр
-        $str = preg_replace('~[^-a-z0-9_]+~u', '-', $str);// заменям все ненужное нам на "-"
+        $str = preg_replace('~[^-a-z0-9_]+~u', '_', $str);// заменям все ненужное нам на "-"
         $str = trim($str, "-");// удаляем начальные и конечные '-'
         return $str;
     }
